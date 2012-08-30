d0604f0a5052b840f2ec35ec4b26e3610ad251ea
Francesco Canessa <makevoid@gmail.com>
2012-08-30 03:36:02 +0200

    Simplest rfid example

--- rfid.rb
+++ rfid.rb
@@ -5,25 +5,18 @@ require "phidgets-ffi"
 
 
 Phidgets::RFID.new do |rfid|
+
   while true do
     rfid.on_tag do |device, tag, obj|
       rfid.led = !rfid.led
       puts "Tag: #{tag}"
     end
-    # puts rfid.tag_present()
+
+    rfid.on_tag_lost do |device, tag, obj|
+      puts "Tag lost: #{tag}"
+    end
+
     sleep 1
   end
-end
 
-# rfid = Phidgets::RFID.new
-# rfid.wait_for_attachment 2000
-#
-# while true do
-#   rfid.on_tag do |device, tag, obj|
-#     rfid.led = !rfid.led
-#     puts "Tag: #{tag}"
-#   end
-#   sleep 1
-# end
-#
-# rfid.close
+end
